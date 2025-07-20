530bf10dd23e496fd8a7b21de2bc0c274a4cbf70
= <fm222753@alumno.udb.edu.sv>
2025-07-19 22:07:08 -0600

    modificacion de prompt (final)

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,20 @@
 'use client';
-
 import { useState, useEffect, useRef } from 'react';
 import ChatWindow from '../components/ChatWindow';
 import InputBox from '../components/InputBox';
 import styles from './page.module.css';
+// Importa la biblioteca de Google Generative AI
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 export default function Home() {
+  // Clave de API de Google Generative AI
   const [apiKey] = useState('AIzaSyDz6lUlv9CDjdBdKWByQ2KGM5bW3C2Rc4g');
+
+  // Estado para almacenar los mensajes del chat
   const [messages, setMessages] = useState([]);
 
+  // Referencia para el contenedor del chat para hacer scroll al final
   const chatWindowRef = useRef(null);
-
   useEffect(() => {
     if (chatWindowRef.current) {
       chatWindowRef.current.scrollTop = chatWindowRef.current.scrollHeight;
@@ -34,7 +37,9 @@ export default function Home() {
     try {
       const startTime = Date.now();
 
+      // Inicializa el cliente de Google Generative AI con la clave de API
       const genAI = new GoogleGenerativeAI(apiKey);
+
       // Se define que el modelo debe responder en formato JSON
       const model = genAI.getGenerativeModel({
         model: 'gemini-2.0-flash',
@@ -44,19 +49,21 @@ export default function Home() {
 
       // Mensaje de sistema para establecer el contexto de Mónica
       const prompt = `
-        Eres Monica, una enfermera virtual. Tu rol es ayudar con temas de salud general, síntomas leves, primeros auxilios y consejos médicos básicos.
-        No debes responder preguntas fuera del ámbito médico. Si te preguntan sobre política, tecnología, deportes o cualquier otro tema, responde educadamente que no puedes responder a eso.
-        Sé profesional, clara, amable y empática. Tu estilo es cercano y respetuoso.
+        Eres Monica, una enfermera virtual de atención primaria. Tu rol es ayudar con temas de salud general, síntomas leves, primeros auxilios y consejos médicos básicos.
+
+        Las especialidades médicas disponibles en la clínica son: pediatría, ginecología, medicina general, nutrición, optometrista, neurologo, cardiologo y dermatología.
+
+        Si te preguntan acerca de los horarios de la clínica son de lunes a viernes de 9:00am a 6:00pm. No debes agendar citas, siempre refiere a una enfermera, sino te preguntan, no lo menciones.
+
+        No debes responder preguntas fuera del ámbito médico. Si te preguntan sobre política, tecnología, deportes o cualquier otro tema, responde educadamente que no puedes responder a eso. Sé profesional, clara, amable y empática. Tu estilo es cercano y respetuoso.
         
         Usuario: ${text}
-        Mónica:
-        
       `;
 
       const result = await model.generateContent(prompt);
       // Devuelve el texto de la respuesta en formato JSON y se convierte a un objeto y se extrae el texto de Mónica
-      // El cual tiene una estructura como { "Mónica": "respuesta" }
-      const responseText = JSON.parse(result.response.text()).Mónica;
+      // El cual tiene una estructura como { "response": "respuesta" }
+      const responseText = await JSON.parse(result.response.text()).response;
 
       // Se imprime la respuesta en la consola para depuración
       console.log(responseText);
@@ -64,6 +71,7 @@ export default function Home() {
       const endTime = Date.now();
       const responseTime = (endTime - startTime) / 1000;
 
+      
       const botMessage = {
         text: responseText,
         sender: 'bot',
